bbf58f1393350104b959d208258ce7d0466bbf57
jangnguyen <jangnguyen@hotmail.com>
2018-04-02 13:56:43 +0700

    Handling Text Input

--- App.js
+++ App.js
@@ -3,97 +3,26 @@ import {StyleSheet, Text, View, TextInput} from 'react-native';
 import {Image} from 'react-native';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
     render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
+
         return (
 
-            <View style={styles.container}>
-
-                {/*Flexbox ghi trên nhánh Learning*/}
-                <View style={{
-                    flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
+            <View style={{padding: 10}}>
+                <TextInput style={{height: 40}}
+                           placeholder={'Type here to translate!'}
+                           onChangeText={(text) => this.setState({text})}/>
+                <Text style={{
+                    padding: 10,
+                    fontSize: 42
                 }}>
-                    <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
-                    <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}/>
-                    <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}/>
-                </View>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                </Text>
             </View>
         );
     }
 }
-
-// Cái này là tự tạo một cái Greeting customize
-class Greeting extends React.Component {
-    render() {
-        return (
-            <Text style={styles.red2}> Hello {this.props.name}!</Text>
-        );
-    }
-}
-
-class Blink extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isShowingText: true};
-
-        setInterval(() => {
-            this.setState(previousState => {
-                return {isShowingText: !previousState.isShowingText};
-            });
-        }, 1000);
-    }
-
-    render() {
-        let display = this.state.isShowingText ? this.props.text : ' ';
-        return (
-            <Text style={[styles.bigblue, styles.red2, styles.violet]}>{display}</Text>
-        );
-    }
-}
-
-const
-    styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#fff',
-            justifyContent: 'center',
-        },
-        view1: {
-            backgroundColor: 'powderblue',
-            flex: 2,
-        },
-        view2: {
-            backgroundColor: '#E984FA',
-            flex: 1,
-        },
-        view3: {
-            backgroundColor: '#FEC32E',
-            flex: 2,
-        },
-        image: {
-            height: 250,
-            flex: 1,
-        },
-        bigblue: {
-            color: 'blue',
-            fontWeight: 'bold',
-            fontSize: 30,
-        },
-        red: {
-            color: 'red',
-        },
-        red2: {
-            color: 'red',
-            fontSize: 16,
-        },
-        violet: {
-            color: '#7C55FB',
-            alignItems: 'center',
-            justifyContent: 'center',
-        }
-    });
