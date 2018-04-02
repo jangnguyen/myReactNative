7380ebd9e7d74133f1f1fa9625c6a5d5bccd135a
jangnguyen <jangnguyen@hotmail.com>
2018-04-02 17:18:59 +0700

    Handling Touch with 4 type of Touchable items

--- App.js
+++ App.js
@@ -1,30 +1,69 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button, Alert} from 'react-native';
+import {
+    Platform,
+    TouchableHighlight,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    TouchableWithoutFeedback
+} from 'react-native';
 
 export default class App extends React.Component {
 
+    /* Bên ngoài JSX*/
     _onPressButton() {
         Alert.alert('You tapped the button!')
     }
 
+    /**/
+    _onLongPressButton() {
+        Alert.alert('Mày vừa bấm long-press đó cu!')
+    }
+
+    /* Ví dụ về comment  bên ngoài JSX*/
     render() {
         return (
+            /*Content chứa*/
             <View style={styles.container}>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        onPress={this._onPressButton}
-                        title="Press me"
-                        color="#841584"/>
-                </View>
-                <View style={styles.alternativeLayoutButtonContainer}>
-                    <Button
-                        onPress={this._onPressButton}
-                        title='This look great!'/>
-                    <Button
-                        onPress={this._onPressButton}
-                        title={'OK!'}
-                        color={"#7C55FB"}/>
-                </View>
+                {/*TouchableHighlight - Chớp 1 cái nhè nhẹ*/}
+                <TouchableHighlight onPress={this._onPressButton} underlayColor={'white'}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            1. TouchableHighlight
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+
+                {/*TouchableOpacity - mờ rồi từ từ trong lại dần*/}
+                <TouchableOpacity onPress={this._onPressButton}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            2. TouchableOpacity
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+
+                {/**/}
+                <TouchableWithoutFeedback
+                    onLongPress={this._onLongPressButton}
+                    underlayColor={'white'}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            3. Touchable without Feedback
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+
+
+                <TouchableNativeFeedback
+                    onPress={this._onPressButton}
+                    background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            4. Touchable Native
+                        </Text>
+                    </View>
+                </TouchableNativeFeedback>
             </View>
         );
     }
@@ -32,15 +71,17 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        paddingTop: 60,
+        alignItems: 'center',
     },
-    buttonContainer: {
-        margin: 20
+    button: {
+        marginBottom: 30,
+        width: 260,
+        alignItems: 'center',
+        backgroundColor: '#FEC32E',
     },
-    alternativeLayoutButtonContainer: {
-        margin:20,
-        flexDirection:'row',
-        justifyContent:'space-between'
+    buttonText: {
+        padding: 20,
+        color: 'white'
     },
 });
