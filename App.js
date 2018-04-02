b659f97d8f58c1f7cec01b4cc7263d80769c9f7c
jangnguyen <jangnguyen@hotmail.com>
2018-04-02 17:34:07 +0700

    Using a FlatView

--- App.js
+++ App.js
@@ -1,40 +1,47 @@
 import React from 'react';
-import {StyleSheet, ScrollView, View, Text, Image} from 'react-native';
+import {StyleSheet, FlatList, View, Text} from 'react-native';
 
 export default class App extends React.Component {
 
     render() {
         return (
-            <ScrollView>
-                <Text style={{fontSize:196}}>Scroll me plz</Text>
-
-                <Text style={{fontSize:196}}>If you like</Text>
-
-                <Text style={{fontSize:196}}>Scrolling down</Text>
-
-                <Text style={{fontSize:196}}>What's the best</Text>
-
-                <Text style={{fontSize:196}}>Framework around?</Text>
-
-                <Text style={{fontSize:180}}>React Native</Text>
-            </ScrollView>
+            <View style={styles.container}>
+                <FlatList
+                    data={[
+                        {key: 'Giang'},
+                        {key: 'Huy'},
+                        {key: 'Phuong'},
+                        {key: 'Quynh'},
+                        {key: 'Hai'},
+                        {key: 'Thuy'},
+                        {key: 'Hong'},
+                        {key: 'Tuan'},
+                        {key: 'Vy'},
+                        {key: 'Ngoc'},
+                        {key: 'Trong'},
+                        {key: 'Hoang'},
+                        {key: 'Quy'},
+                        {key: 'Linh'},
+                        {key: 'Thanh'},
+                        {key: 'Tien'},
+                        {key: 'Thang'},
+                        {key: 'Tan'},
+                        {key: 'Nhi'},
+                    ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 60,
-        alignItems: 'center',
+        flex: 1,
+        paddingTop: 1,
     },
-    button: {
-        marginBottom: 30,
-        width: 260,
-        alignItems: 'center',
-        backgroundColor: '#FEC32E',
-    },
-    buttonText: {
-        padding: 20,
-        color: 'white'
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
     },
 });
