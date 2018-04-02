7e7ca0a74220a861dae86708fb75ca50a7539386
jangnguyen <jangnguyen@hotmail.com>
2018-04-02 15:32:03 +0700

    Handling Touch

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
-import {Image} from 'react-native';
+import {StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: ''};
+
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
     }
 
     render() {
-
         return (
-
-            <View style={{padding: 10}}>
-                <TextInput style={{height: 40}}
-                           placeholder={'Type here to translate!'}
-                           onChangeText={(text) => this.setState({text})}/>
-                <Text style={{
-                    padding: 10,
-                    fontSize: 42
-                }}>
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press me"
+                        color="#841584"/>
+                </View>
+                <View style={styles.alternativeLayoutButtonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title='This look great!'/>
+                    <Button
+                        onPress={this._onPressButton}
+                        title={'OK!'}
+                        color={"#7C55FB"}/>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        margin: 20
+    },
+    alternativeLayoutButtonContainer: {
+        margin:20,
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+});
